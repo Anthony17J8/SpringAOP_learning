f0281a74a8bcb345756c0477d471b4c797a307f7
Kiselev Anton <j8kiselev@gmail.com>
2018-11-12 21:14:42 +0300

    Match only addAcount method of AccountDAO class

--- src/main/java/spring/aop/aspects/DemoLoggingAspect.java
+++ src/main/java/spring/aop/aspects/DemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class DemoLoggingAspect {
 
      /* @Before advice
      call this method for any execution method that has a signature below (point cut expression) */
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void spring.aop.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n ->>> Executing @Before advice on the method addAccount()");
     }
