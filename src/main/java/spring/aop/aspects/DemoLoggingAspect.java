85257716a7f8c0c0018d5e596849ddc42c307e16
Kiselev <j8kiselev@gmail.com>
2018-11-18 18:56:25 +0300

    Match method with any number of parameters

--- src/main/java/spring/aop/aspects/DemoLoggingAspect.java
+++ src/main/java/spring/aop/aspects/DemoLoggingAspect.java
@@ -10,9 +10,9 @@ public class DemoLoggingAspect {
 
     // this is where we add all of related advices for logging
 
-     /* @Before advice
-     call this method for any execution method that has a signature below (point cut expression) */
-    @Before("execution(public void add*(spring.aop.Account, ..))")
+    /* @Before advice
+    call this method for any execution method that has a signature below (point cut expression) */
+    @Before("execution(public void add*(..))")
     public void beforeAddAccountAdvice() {
         System.out.println("\n ->>> Executing @Before advice on the method addAccount()");
     }
