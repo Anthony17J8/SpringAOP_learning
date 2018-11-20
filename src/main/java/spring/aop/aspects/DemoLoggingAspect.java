827f5f589e7f8f5b3fb8ae64fec424444d8dacc5
Kiselev <j8kiselev@gmail.com>
2018-11-20 20:03:08 +0300

    Using AOP Pointcut Declarations

--- src/main/java/spring/aop/aspects/DemoLoggingAspect.java
+++ src/main/java/spring/aop/aspects/DemoLoggingAspect.java
@@ -2,6 +2,7 @@ package spring.aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,11 +11,19 @@ public class DemoLoggingAspect {
 
     // this is where we add all of related advices for logging
 
+    @Pointcut("execution(* spring.aop.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
     /* @Before advice
     call this method for any execution method that has a signature below (point cut expression) */
-    @Before("execution(* spring.aop.dao.*.*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n ->>> Executing @Before advice on the method addAccount()");
     }
 
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n ->>> Performing API analytics");
+    }
 }
